327c4be5914d193f72c749187c28604fc55f7aaa
Sabrina Gomes Bezerra <sabrinagbdev@gmail.com>
2025-01-04 12:32:58 -0300

    feat: add CartItem model and migration

---
+++ app/models/cart_item.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CartItem < ApplicationRecord
+  belongs_to :cart
+  belongs_to :product
+
+  validates :quantity, numericality: { greater_than: 0 }
+
+  def total_price
+    product.price * quantity
+  end
+end
