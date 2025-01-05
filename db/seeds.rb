adc47dd6ca5d29ed233a4816d5dc1356fb62261a
Sabrina Gomes Bezerra <sabrinagbdev@gmail.com>
2025-01-05 09:11:11 -0300

    feat: add more items to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,18 @@
 #   end
 
 
-Product.create([
-  {
-    name: 'Samsung Galaxy S24 Ultra',
-    price: 12999.99
-  },
-  {
-    name: 'iPhone 15 Pro Max',
-    price: 14999.99
-  },
-  {
-    name: 'Xiamo Mi 27 Pro Plus Master Ultra',
-    price: 999.99
-  }
-])
+[
+  { name: 'Samsung Galaxy S24 Ultra', price: 12999.99 },
+  { name: 'iPhone 15 Pro Max', price: 14999.99 },
+  { name: 'Xiamo Mi 27 Pro Plus Master Ultra', price: 999.99 },
+  { name: 'iPad Pro 2023', price: 1999.99 },
+  { name: 'Samsung Galaxy Tab S8', price: 1299.99 },
+  { name: 'Xiamo Mi Pad 6', price: 499.99 },
+  { name: 'Samsung Galaxy Watch 5', price: 599.99 },
+  { name: 'Apple Watch 10', price: 799.99 },
+].each do |product_data|
+  Product.find_or_create_by(name: product_data[:name]) do |product|
+    product.price = product_data[:price]
+  end
+end
+
